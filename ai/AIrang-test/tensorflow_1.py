73a526cdbe2758482ed96b40b92c93960710095f
https://lymchgmk@gmail.com@lab.ssafy.com <https://lymchgmk@gmail.com@lab.ssafy.com>
2020-09-09 17:33:21 +0900

    test:

--- ai/AIrang-test/tensorflow_1.py
+++ ai/AIrang-test/tensorflow_1.py
@@ -1,1 +1,9 @@
-asdfasdf
+import face_recognition
+from matplotlib import pyplot
+
+image = face_recognition.load_image_file("AIrang/images/ariel.jpg")
+face_locations = face_recognition.face_locations(image)
+
+pyplot.rcParams["figure.figsize"] = (16, 16)
+pyplot.imshow(image)
+pyplot.show()
