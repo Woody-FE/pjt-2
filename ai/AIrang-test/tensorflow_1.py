e7bb73c4aacf2712ddb8c094e2777af9c86f17cf
https://lymchgmk@gmail.com@lab.ssafy.com <https://lymchgmk@gmail.com@lab.ssafy.com>
2020-09-10 11:16:39 +0900

    4k 2d 얼굴인식 안됨 확인

--- ai/AIrang-test/tensorflow_1.py
+++ ai/AIrang-test/tensorflow_1.py
@@ -1,7 +1,8 @@
 import face_recognition
 from matplotlib import pyplot
 
-image = face_recognition.load_image_file("AIrang/images/ariel.jpg")
+image = face_recognition.load_image_file(
+    "AIrang/images/snowwhite_5120x3200.jpg")
 face_locations = face_recognition.face_locations(image)
 
 pyplot.rcParams["figure.figsize"] = (16, 16)
