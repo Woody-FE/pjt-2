c1b89e645734bd9fa6a16594c842f3eb579fc811
https://lymchgmk@gmail.com@lab.ssafy.com <https://lymchgmk@gmail.com@lab.ssafy.com>
2020-09-10 12:21:03 +0900

    face_recognition examples

--- ai/AIrang-test/tensorflow_1.py
+++ ai/AIrang-test/tensorflow_1.py
@@ -1,10 +1,28 @@
+import cv2
+import os
 import face_recognition
+from PIL import Image, ImageDraw
+# from IPython.display import Image, display
 from matplotlib import pyplot
 
 image = face_recognition.load_image_file(
-    "AIrang/images/snowwhite_5120x3200.jpg")
+    "AIrang/images/family2.jpg")
 face_locations = face_recognition.face_locations(image)
+face_landmarks_list = face_recognition.face_landmarks(image)
 
-pyplot.rcParams["figure.figsize"] = (16, 16)
-pyplot.imshow(image)
+pil_image = Image.fromarray(image)
+for face_landmarks in face_landmarks_list:
+    d = ImageDraw.Draw(pil_image, 'RGBA')
+    # 이미지 색칠
+
+
+# pil_image.show()
+
+# 얼굴인식 확인
+# for (top, right, bottom, left) in face_locations:
+#     cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 1)
+
+# pyplot.rcParams["figure.figsize"] = (16, 16)
+# pyplot.imshow(image)
+pyplot.imshow(pil_image)
 pyplot.show()
