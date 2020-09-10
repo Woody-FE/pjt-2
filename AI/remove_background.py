052aa0c4bcdc09915a19c099fccdf299e04f5dad
JJS <cjs1216@gmail.com>
2020-09-10 23:11:06 +0900

    S03P22D105-12 | modified

--- AI/remove_background.py
+++ AI/remove_background.py
@@ -4,38 +4,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# image_bgr = cv2.imread('images/91.jpg')
-# image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-
-# # 사각형 좌표: 시작점의 x,y  ,넢이, 너비
-# rectangle = (0, 56, 256, 150)
-
-# # 초기 마스크 생성
-# mask = np.zeros(image_rgb.shape[:2], np.uint8)
-
-# # grabCut에 사용할 임시 배열 생성
-# bgdModel = np.zeros((1, 65), np.float64)
-# fgdModel = np.zeros((1, 65), np.float64)
-
-# # grabCut 실행
-# cv2.grabCut(image_rgb, # 원본 이미지
-#         mask,       # 마스크
-#         rectangle,  # 사각형
-#         bgdModel,   # 배경을 위한 임시 배열
-#         fgdModel,   # 전경을 위한 임시 배열 
-#         5,          # 반복 횟수
-#         cv2.GC_INIT_WITH_RECT) # 사각형을 위한 초기화
-
-# # 배경인 곳은 0, 그 외에는 1로 설정한 마스크 생성
-# mask_2 = np.where((mask==2) | (mask==0), 0, 1).astype('uint8')
-
-# # 이미지에 새로운 마스크를 곱행 배경을 제외
-# image_rgb_nobg = image_rgb * mask_2[:, :, np.newaxis]
-
-# # plot
-# plt.imshow(image_rgb_nobg)
-# plt.show()
-
 image = face_recognition.load_image_file("./images/91.jpg")
 face_locations = face_recognition.face_locations(image)
 
@@ -51,3 +19,29 @@ for face_location in face_locations:
     face_image = image[top:bottom, left:right]
     pil_image = Image.fromarray(face_image)
     pil_image.show()
+    pil_image.save("./images/baby.jpg")
+
+# 얼굴에서 배경제거 코드 구현
+# 얼굴 동화풍으로 transfer 코드 구현
+
+# im = cv2.imread("./images/baby.jpg")
+# cv2.imshow('original',im)
+# mask = np.zeros(im.shape[:2],np.uint8)
+# rect = (box[0][0], box[0][1], box[0][2]-box[0][0], box[0][3]-box[0][1])
+# bgdModel = np.zeros((1,65),np.float64)
+# fgdModel = np.zeros((1,65),np.float64)
+# cv2.grabCut(im,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+# if len(np.where((mask==3)|(mask==1))[0])>0:
+#     mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+#     mask2 = np.repeat(mask2[:,:,np.newaxis],3,axis=2)
+# else:
+#     mask2 = np.zeros_like(im)
+#     mask2[box[0][1]:box[0][3],box[0][0]:box[0][2],:] = 1
+# im2 = im*mask2
+# cv2.imshow('post-grabcut',im2)
+# minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(mask)
+# flag, mask = cv2.threshold(mask, maxVal-1, 255, cv2.cv.CV_THRESH_BINARY)
+# cv2.imshow("mask", mask)
+# b, g, r = cv2.split(im2)
+# img_RGBA = cv2.merge((b, g, r, mask))
+# cv2.imshow("final",img_RGBA)
