106bdcdde95e5acdba15bb51f0c395bbed9c505d
https://lymchgmk@gmail.com@lab.ssafy.com <https://lymchgmk@gmail.com@lab.ssafy.com>
2020-09-14 15:47:54 +0900

    update nukki

--- AI/AIrang-test/nukki_by_color.py
+++ AI/AIrang-test/nukki_by_color.py
@@ -1,10 +1,18 @@
 from PIL import Image
 # 누끼컷으로 변경할 이미지
-img = Image.open('./iwantnuki.png')
+img = Image.open('./face0.jpg')
 img = img.convert("RGBA")
 datas = img.getdata()
+print(datas)
 newData = []
 for item in datas:
+    # 여기에 face_recognition 좌표로 조건
     if item[0] == 255 and item[1] == 255 and item[2] == 255:
-    newData.append((255, 255, 255, 0)) else: newData.append(item) img.putdata(newData)
-    # png 확장자로 저장 img.save("./TransparentImage.png", "PNG")
+        newData.append((255, 255, 255, 0))
+
+    else:
+        newData.append(item)
+        img.putdata(newData)
+
+    # png 확장자로 저장
+    img.save("./NukkiedImage.png", "PNG")
