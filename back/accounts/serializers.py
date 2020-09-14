1501d337e06b10e8994a774398489c57df796077
JongWoo <jongwoo328@gmail.com>
2020-09-14 13:35:49 +0900

    S03P22D105-14 | user create complete

--- back/accounts/serializers.py
+++ back/accounts/serializers.py
@@ -6,17 +6,11 @@ from rest_framework import serializers
 User = get_user_model()
 
 class CreateUserSerializer(serializers.ModelSerializer):
-    child_name = serializers.CharField(required=False)
-    child_gender = serializers.CharField(required=False)
-    child_image = serializers.ImageField(required=False)
+    username = serializers.CharField(required=False)
     class Meta:
         model = User
         fields = (
             'username',
             'email',
-            'password1',
-            'password2',
-            'child_name',
-            'child_gender',
-            'child_image',
+            'password',
         )
