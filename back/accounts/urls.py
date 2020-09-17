4db6973ff5bc65fb18751a5cb2cb2d4af1b9eef5
JongWoo <jongwoo328@gmail.com>
2020-09-17 13:52:35 +0900

    S03P22D105-29 | complete family crud

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -8,5 +8,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('<int:user_id>/', views.UserDetailView.as_view()),
     path('<int:user_id>/child/image/', views.UserImageUpdateView.as_view()),
-    # path('<int:user_id>/family/',)
+    path('<int:user_id>/family/', views.UserFamilyView.as_view()),
+    path('<int:user_id>/family/<int:family_id>/', views.UserFamilyDetailView.as_view()),
 ]
