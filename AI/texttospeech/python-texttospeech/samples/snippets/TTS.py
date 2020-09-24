2870d2a6120df805f18d82d1dda26c102c51a3f2
JJS <cjs1216@gmail.com>
2020-09-24 12:53:48 +0900

    S03P23D105-23 | add file path return code

---
+++ AI/texttospeech/python-texttospeech/samples/snippets/TTS.py
@@ -0,0 +1,41 @@
+import os.path
+
+def TTS(sentence,output):
+    from google.cloud import texttospeech
+
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=sentence)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="ko-KR",
+        name="ko-KR-Standard-A",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open(output, "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file {}'.format(output))
+    # [END tts_quickstart]
+    
+    return os.path.abspath(output)
+
+path = TTS('목요일 점심입니다. 오늘 하루도 파이팅하시고 즐겁게 코딩합시다.', 'fighting.mp3')
+print(path)
