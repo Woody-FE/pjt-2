a7e9c689166616b667d366c2b312612ddc806c0c
JongWoo <jongwoo328@gmail.com>
2020-09-26 15:48:46 +0900

    S03P23D105-25 | modify mystorycreateserializer

--- back/tts/views.py
+++ back/tts/views.py
@@ -12,8 +12,8 @@ def create_voice(request):
     scripts = Script.objects.all()
     for script in scripts:
         s = script.content
-        s = s.replace('<br>','')
-        if not '{child_name}' in s:
+        s = s.replace('<br>','.')
+        if not '{child_name}' in s and script.character.id == 2:
             TTS(s,f'script_{script.id}_voice.mp3')
     
     return Response(status=status.HTTP_200_OK)
