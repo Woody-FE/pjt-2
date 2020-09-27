e66849d3cf9444ecf5c534c5c2c0ae3c1738b21f
JongWoo <jongwoo328@gmail.com>
2020-09-27 21:06:24 +0900

    S03P23D105-25 | modify storyimage model

--- back/tts/views.py
+++ back/tts/views.py
@@ -12,8 +12,8 @@ def create_voice(request):
     scripts = Script.objects.all()
     for script in scripts:
         s = script.content
-        s = s.replace('<br>','')
+        s = s.replace('<br>','.')
         if not '{child_name}' in s:
-            TTS(s,f'script_{script.id}_voice.mp3')
+            TTS(s,f'./character_voice/story/1/나레이션/script_{script.id}.mp3')
     
     return Response(status=status.HTTP_200_OK)
