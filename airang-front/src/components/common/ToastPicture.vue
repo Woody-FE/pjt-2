f0ffbda71e80f353f2083c4275e8c59faeba188a
Taein Kim <kti91@likelion.org>
2020-10-04 22:34:25 +0900

    S03P23D105-5 | updated

--- airang-front/src/components/common/ToastPicture.vue
+++ airang-front/src/components/common/ToastPicture.vue
@@ -1,6 +1,7 @@
 <template>
 	<section class="toast" :class="toastAnimationClass">
-		<h2>책을 저장하시겠습니까 ?</h2>
+		<img src="" alt="" />
+		<h2 class="toast-title">책을 저장하시겠습니까 ?</h2>
 		<section>
 			<button class="toast-btn-white" @click="closeToast">취소</button>
 			<button class="toast-btn-purple" @click="showToast">저장</button>
@@ -59,14 +60,13 @@ export default {
 <style lang="scss" scoped>
 .toast {
 	position: fixed;
-	/* width: 600px; */
 	width: 100vw;
-	@media screen and (max-width: 640px) {
+	/* @media screen and (max-width: 640px) {
 		width: 500px;
-	}
-	/* height: 600px; */
+	} */
 	height: 100vh;
-	background-color: #22252e;
+	background: white;
+	color: black;
 	border-radius: 4px;
 	box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.2);
 	color: white;
@@ -77,32 +77,8 @@ export default {
 	justify-content: center;
 	align-items: center;
 	transform: translate(-50%, -50%);
-	.toast-img {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 50%;
-		.toast-img__box {
-			width: 35%;
-			height: 100%;
-			box-sizing: border-box;
-			padding: 1rem;
-		}
-		.toast-img__description {
-			width: 65%;
-			height: 100%;
-			color: white;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			box-sizing: border-box;
-			padding-left: 1rem;
-			.mystory-description__title {
-				text-align: center;
-				margin-bottom: 1rem;
-			}
-		}
+	.toast-title {
+		font-size: 3rem;
 	}
 }
 .toast.none {
@@ -110,25 +86,32 @@ export default {
 }
 .toast-btn-white {
 	border: none;
+	width: 7rem;
 	border-radius: 3px;
 	padding: 0 1rem;
-	font-size: 1rem;
+	font-size: 2rem;
 	font-weight: 700;
 	background: white;
 	color: black;
-	height: 2rem;
+	height: 4rem;
 	margin-top: 1.5rem;
 	margin-right: 0.5rem;
+	cursor: pointer;
+	z-index: 2;
 }
 .toast-btn-purple {
 	border: none;
 	border-radius: 3px;
+	width: 7rem;
 	padding: 0 1rem;
-	font-size: 1rem;
+	font-size: 2rem;
 	font-weight: 700;
+	background: $orange;
 	color: white;
-	height: 2rem;
+	height: 4rem;
 	margin-top: 1.5rem;
 	margin-left: 0.5rem;
+	cursor: pointer;
+	z-index: 2;
 }
 </style>
